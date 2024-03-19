a49bedfc71d09693186feb5b718a94282a6021b9
topdefi <94203269+topdefi@users.noreply.github.com>
2024-03-19 12:39:03 -0700

    Remove

--- src/merkle/mod.rs
+++
@@ -1,6 +0,0 @@
-//! Merkle types.
-
-pub mod nibble;
-mod node;
-
-pub use node::{empty_nodes, Branch, Extension, Leaf, MerkleNode, MerkleValue};
--- src/ops/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod build;
-pub mod delete;
-pub mod get;
-pub mod insert;
-
-pub mod debug;
-pub mod diff;
